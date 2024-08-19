3720e16fa7ff92fec77bb96d6132f616d6f89a06
Shivam Mishra <shivammishra15092003@gmail.com>
2024-08-19 18:44:43 +0530

    Time: 0 ms (100%), Space: 7.4 MB (71.95%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int ans=0;
+        for(int i=2;i<=n;i++){
+            while(n%i==0){
+                ans+=i;
+                n/=i;
+            }
+        }
+        return ans;
+    }
+};
