ad9e815268ae7913098169862ea61de014c0e18a
Shivam Mishra <shivammishra15092003@gmail.com>
2024-09-25 22:54:05 +0530

    Time: 649 ms (29.42%), Space: 704.2 MB (63.84%) - LeetHub

---
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    struct TrieNode {
+        TrieNode* children[26] = {};
+        int count = 0;
+    };
+    vector<int> sumPrefixScores(vector<string>& words) {
+        TrieNode* root = new TrieNode();
+        for (const string& word : words) {
+            TrieNode* node = root;
+            for (char c : word) {
+                if (!node->children[c - 'a']) {
+                    node->children[c - 'a'] = new TrieNode();
+                }
+                node = node->children[c - 'a'];
+                node->count++;
+            }
+        }
+        vector<int> res;
+        for (const string& word : words) {
+            TrieNode* node = root;
+            int sum = 0;
+            for (char c : word) {
+                node = node->children[c - 'a'];
+                sum += node->count;
+            }
+            res.push_back(sum);
+        }
+        return res;
+    }
+};
