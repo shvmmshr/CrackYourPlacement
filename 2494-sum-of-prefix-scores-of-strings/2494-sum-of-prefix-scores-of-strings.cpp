0e058e6ca8430b3a650a77520b5e51369b69e0ee
Shivam Mishra <shivammishra15092003@gmail.com>
2024-09-25 23:04:55 +0530

    Time: 694 ms (24.68%), Space: 704.2 MB (57.11%) - LeetHub

--- 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     struct TrieNode {
-        unordered_map<char, TrieNode*> children;
+        TrieNode* children[26] = {};
         int count = 0;
     };
+    
     vector<int> sumPrefixScores(vector<string>& words) {
         TrieNode* root = new TrieNode();
         
         for (const string& word : words) {
             TrieNode* node = root;
             for (char c : word) {
-                if (node->children.find(c) == node->children.end()) {
-                    node->children[c] = new TrieNode();
+                if (!node->children[c - 'a']) {
+                    node->children[c - 'a'] = new TrieNode();
                 }
-                node = node->children[c];
+                node = node->children[c - 'a'];
                 node->count++;
             }
         }
@@ -23,7 +24,7 @@ public:
             TrieNode* node = root;
             int sum = 0;
             for (char c : word) {
-                node = node->children[c];
+                node = node->children[c - 'a'];
                 sum += node->count;
             }
             res.push_back(sum);
