7b6832b653251be59c93bdf976c763a320174031
Shivam Mishra <shivammishra15092003@gmail.com>
2024-09-29 19:10:46 +0530

    Time: 2878 ms (5.09%), Space: 48.3 MB (99.88%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -0,0 +1,38 @@
+class AllOne {
+public:
+    unordered_map<string, int> freq;
+    AllOne() {
+    }
+    
+    void inc(string key) {
+        freq[key]++;
+    }
+    
+    void dec(string key) {
+        freq[key]--;
+    }
+    
+    string getMaxKey() {
+        int mx = 0;
+        string ans = "";
+        for(auto& val:freq){
+            if(val.second > mx){
+                mx = val.second;
+                ans = val.first;
+            }
+        }
+        return ans;
+    }
+    
+    string getMinKey() {
+        int mini = INT_MAX;
+        string ans = "";
+        for(auto& val:freq){
+            if(val.second!=0 and val.second < mini){
+                mini = val.second;
+                ans = val.first;
+            }
+        }
+        return ans;
+    }
+};
