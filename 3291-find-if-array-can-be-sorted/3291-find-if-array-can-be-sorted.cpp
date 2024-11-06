4a9bc10315a59138907cf1c6bb09b00cea268371
Shivam Mishra <shivammishra15092003@gmail.com>
2024-11-06 23:09:38 +0530

    Time: 1 ms (56.83%), Space: 31.7 MB (33.78%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <bitset>
+
+class Solution {
+public:
+    bool canSortArray(std::vector<int>& nums) {
+        int n = nums.size();
+        vector<int> values = nums;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (values[j] <= values[j + 1]) {
+                    continue;
+                } else {
+                    int countJ = bitset<32>(values[j]).count();
+                    int countJ1 = bitset<32>(values[j + 1]).count();
+                    if (countJ == countJ1) {
+                        swap(values[j], values[j + 1]);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+};
