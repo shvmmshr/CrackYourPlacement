d018700f086e8109848f0882c320d945f3063aa7
Shivam Mishra <shivammishra15092003@gmail.com>
2024-11-19 21:32:10 +0530

    Time: 106 ms (75.33%), Space: 95.2 MB (57.32%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -4,19 +4,13 @@ public:
         unordered_map<int, int> freq;
         long long sum = 0;
         long long maxsum = 0;
-        
         int n = nums.size();
         if (n < k) return 0;
-        
         for (int i = 0; i < k; i++) {
             freq[nums[i]]++;
             sum += nums[i];
         }
-
-        if (freq.size() == k) {
-            maxsum = sum;
-        }
-
+        if (freq.size() == k) maxsum = sum;
         for (int i = k; i < n; i++) {
             int left = nums[i - k];
             freq[left]--;
@@ -24,11 +18,9 @@ public:
             if (freq[left] == 0) {
                 freq.erase(left);
             }
-
             int right = nums[i];
             freq[right]++;
             sum += right;
-
             if (freq.size() == k) {
                 maxsum = max(maxsum, sum);
             }
