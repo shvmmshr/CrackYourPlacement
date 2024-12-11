233e80bdddce67c9f5cc3817e2b2272a2ab54286
Shivam Mishra <shivammishra15092003@gmail.com>
2024-12-11 23:27:03 +0530

    Time: 45 ms (90.2%), Space: 48.5 MB (88.12%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,22 @@
+/**
+ * @param {any} val
+ * @return {Object}
+ */
+var expect = function(val) {
+  return {
+    toBe: function(otherVal) {
+      if (val === otherVal) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: function(otherVal) {
+      if (val !== otherVal) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    }
+  };
+};
