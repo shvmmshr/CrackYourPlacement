742dd485317fd93dc1011b42728cb4032bd47e79
Shivam Mishra <shivammishra15092003@gmail.com>
2024-12-13 18:55:48 +0530

    Time: 55 ms (93.86%), Space: 98.7 MB (90.85%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,25 +1,22 @@
-
 class Solution {
 public:
     long long findScore(vector<int>& nums) {
         vector<pair<int, int>> numWithIndex;
+        numWithIndex.reserve(nums.size());
         long long sum = 0;
-        vector<int> marked;
-
+        vector<bool> marked(nums.size(), false);
         for (int i = 0; i < nums.size(); i++) {
             numWithIndex.emplace_back(nums[i], i);
         }
-        
         sort(numWithIndex.begin(), numWithIndex.end());
-        
-        for (const auto& pair : numWithIndex) {
+        for (auto& pair : numWithIndex) {
             int num = pair.first;
             int idx = pair.second;
-            if (find(marked.begin(), marked.end(), idx) == marked.end()) {
+            if (!marked[idx]) {
                 sum += num;
-                marked.push_back(idx);
-                if (idx > 0) marked.push_back(idx - 1);
-                if (idx < nums.size() - 1) marked.push_back(idx + 1);
+                marked[idx] = true;
+                if (idx > 0) marked[idx - 1] = true;
+                if (idx < nums.size() - 1) marked[idx + 1] = true;
             }
         }
 
