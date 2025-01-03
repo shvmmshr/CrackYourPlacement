6ad1bef71d13a384feaab47bd104d94fa4012d30
Shivam Mishra <shivammishra15092003@gmail.com>
2025-01-03 21:24:05 +0530

    Time: 0 ms (100%), Space: 89.4 MB (62.32%) - LeetHub

---
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        int res = 0;
+        long long totalSum = 0, leftSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        } 
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            leftSum += nums[i];
+            if (leftSum >= totalSum - leftSum) {
+                res++;
+            }
+        }   
+        return res;
+    }
+};
