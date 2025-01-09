1704fb2c3478899b2fd695860d7004c3b2d1b90d
Shivam Mishra <shivammishra15092003@gmail.com>
2025-01-09 23:31:25 +0530

    Time: 1 ms (33.99%), Space: 13.3 MB (60.46%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -3,7 +3,7 @@ public:
     int prefixCount(vector<string>& arr, string pref) {
         int ans = 0;
         int n = pref.size();
-        for(auto it:arr){
+        for(auto &it:arr){
             if(it.size()>=n&&pref == it.substr(0,n)){
                 ans++;
             }
