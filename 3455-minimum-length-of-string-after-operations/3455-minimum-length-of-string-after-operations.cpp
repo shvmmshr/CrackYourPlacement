5fbf614ac67218e360010380f213cfaf935471c3
Shivam Mishra <shivammishra15092003@gmail.com>
2025-01-13 23:26:46 +0530

    Time: 110 ms (26.21%), Space: 32.6 MB (21.37%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -3,10 +3,10 @@ public:
     int minimumLength(string s) {
         int res=0;
         map<char,int> mp;
-        for(auto it:s){
+        for(auto &it:s){
             mp[it]++;
         }
-        for(auto it: mp){
+        for(auto &it: mp){
             it.second=it.second%2;
             res+=(it.second==0)?2:1;
         }
