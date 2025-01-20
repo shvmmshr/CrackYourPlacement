760d0baf74821eb34508a74cd63ada0d15426b0f
Shivam Mishra <shivammishra15092003@gmail.com>
2025-01-20 23:55:30 +0530

    Time: 100 ms (55.79%), Space: 170.5 MB (48.48%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size(), N = m * n;
-        vector<int> numToIndex(N + 1, -1);  
-        for (int i = 0; i < N; ++i) {
-            numToIndex[arr[i]] = i;
-        }
-        vector<int> rowPainted(m, 0), colPainted(n, 0);
-        vector<int> rowPos(N), colPos(N);
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int num = mat[i][j];
-                int indexInArr = numToIndex[num];
-                rowPos[indexInArr] = i;
-                colPos[indexInArr] = j;
+        int rows = mat.size();
+        int cols = mat[0].size();
+        unordered_map<int, pair<int, int>> ump;
+        vector<int> row_cnt(rows);
+        vector<int> col_cnt(cols);
+        for(int r=0; r<rows; r++){
+            for(int c=0; c<cols; c++){
+                int val = mat[r][c];
+                ump[val] = {r, c};
             }
         }
-        for (int i = 0; i < N; ++i) {
+        for(int i=0; i<arr.size(); i++){
             int num = arr[i];
-            int rowIdx = rowPos[numToIndex[num]];
-            int colIdx = colPos[numToIndex[num]];
-            if (++rowPainted[rowIdx] == n || ++colPainted[colIdx] == m) {
+            auto [r, c] = ump[num];
+            row_cnt[r]++;
+            col_cnt[c]++;
+            if(row_cnt[r] == cols || col_cnt[c] == rows){
                 return i;
             }
         }
-
         return -1;
     }
 };
