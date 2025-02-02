2e73a69e8b4703d32f36fe3f918ae43192703733
Shivam Mishra <shivammishra15092003@gmail.com>
2025-02-02 22:37:43 +0530

    Time: 0 ms (100%), Space: 11.2 MB (22.96%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -6,7 +6,7 @@ public:
         
         for (int i = 0; i < n;i++) {
             if (nums[i] > nums[(i + 1) % n]) {
-                ++count;
+                count++;
             }
             if (count > 1) {
                 return false;
