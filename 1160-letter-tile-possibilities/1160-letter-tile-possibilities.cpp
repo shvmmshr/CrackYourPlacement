88c15f6bc18c8d4d1d47fc0205e4713f9177f7f9
Shivam Mishra <shivammishra15092003@gmail.com>
2025-02-18 00:05:43 +0530

    Time: 0 ms (100%), Space: 7.9 MB (92.23%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    int Perm(int n, vector<int>& freq, int fz){
-        if (n==1)
-           return fz-count(freq.begin(), freq.end(), 0);
-        int ans=0;
-        for (int i=0; i<fz; i++) {
-            if (freq[i]>0) {
-                freq[i]--; 
-                ans+= Perm(n-1, freq, fz);
-                freq[i]++;
+    int Perm(int n, vector<int>& cnt, int k) {
+        if (n == 1)
+            return k - count(cnt.begin(), cnt.end(), 0);
+        
+        int res = 0;
+        for (int i = 0; i < k; i++) {
+            if (cnt[i] > 0) {
+                cnt[i]--;
+                res += Perm(n - 1, cnt, k);
+                cnt[i]++;
             }
         }
-        return ans;
+        return res;
     }
+
     int numTilePossibilities(string& tiles) {
-        int sz=0, tz=tiles.size();
-        vector<int> freq(26, 0);
-        for (char c: tiles)
-            if (++freq[c-'A']==1) sz++;
-        sort(freq.begin(), freq.end(), greater<int>());
-        freq.resize(sz);
+        int k = 0, n = tiles.size();
+        vector<int> cnt(26, 0);
+        
+        for (char c : tiles)
+            if (++cnt[c - 'A'] == 1) k++;
+        
+        sort(cnt.begin(), cnt.end(), greater<int>());
+        cnt.resize(k);
     
-        int cnt=0;
-        for (int len=1; len<=tz; len++) 
-            cnt+= Perm(len, freq, sz);
-        return cnt;
+        int ans = 0;
+        for (int len = 1; len <= n; len++) 
+            ans += Perm(len, cnt, k);
+        
+        return ans;
     }
 };
